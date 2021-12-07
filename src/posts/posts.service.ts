11fd131d5420bed2d914708fb83ea42fdce852d5
Owen Brice Indrianarison <indrianarison.brice@gmail.com>
2021-12-07 15:33:45 +0100

    Setting up a PostgreSQL database with TypeORM

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,47 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import Post from './models/post.entity';
+
+@Injectable()
+export class PostsService {
+  constructor(
+    @InjectRepository(Post) private readonly postsRepository: Repository<Post>,
+  ) {}
+
+  getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  getPostById(id: number) {
+    const post = this.postsRepository.findOne(id);
+    if (post) {
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
+    return newPost;
+  }
+
+  async updatePost(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne(id);
+    if (updatedPost) {
+      return updatedPost;
+    }
+    throw new HttpException('Post Not Found', HttpStatus.NOT_FOUND);
+  }
+
+  async deletePost(id: number) {
+    const deleteResponse = await this.postsRepository.delete(id);
+    if (!deleteResponse.affected) {
+      throw new HttpException('Post Not Found', HttpStatus.NOT_FOUND);
+    }
+  }
+}
